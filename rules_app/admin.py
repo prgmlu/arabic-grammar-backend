b5c053baa5afa1dac5ac120a9029d61d4f2a5f63
mostafa elkabany <mostafa@ardentpeak.com>
2023-06-13 17:29:53 +0300

    highlighting widget in the admin panel

--- rules_app/admin.py
+++ rules_app/admin.py
@@ -1,5 +1,13 @@
+# rules_app/admin.py
 from django.contrib import admin
 from .models import Rule, Example
+from .forms import ExampleForm
+
+class ExampleAdmin(admin.ModelAdmin):
+    form = ExampleForm
+
+    class Media:
+        js = ('example_admin.js',)  # Path to your JavaScript file
 
 admin.site.register(Rule)
-admin.site.register(Example)
+admin.site.register(Example, ExampleAdmin)
